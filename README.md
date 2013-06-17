794dfef2e0bb2ed37c144b2a924c6e68afbe63fa
David Négrier <d.negrier@thecodingmachine.com>
2013-06-17 12:57:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+jqBootstrapValidation installer
+===============================
+
+This is a simple Composer package that will install jqBootstrapValidation in your Composer project.
+It will also create a "Mouf" instance for that plugin (if you are using Mouf).
+
---
+++ install.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__."/../../autoload.php";
+
+use Mouf\Actions\InstallUtils;
+use Mouf\MoufManager;
+use Mouf\Html\Utils\WebLibraryManager\WebLibraryInstaller;
+
+// Let's init Mouf
+InstallUtils::init(InstallUtils::$INIT_APP);
+
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+
+WebLibraryInstaller::installLibrary("javascript.jqBootstrapValidation",
+array(
+'vendor/mouf/javascript.jqBootstrapValidation/lib/jqBootstrapValidation.js'
+		), array(),	array(), false
+);
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
+?>
